73b1989315e8d5c4fe684354a20400a71590ab12
perica22 <pprokic22@gmail.com>
2019-07-30 15:50:11 +0200

    ..

--- app/routes.py
+++ app/routes.py
@@ -4,103 +4,70 @@ from app import ENVIRONMENT
 
 from flask import request, jsonify
 
-from app.tree_service import Tree, TREE_FILES
+from app.tree_service import Tree
 import ipdb
 
 
 @APP.route("/search", methods=["POST"])
 def search():
-    if not ENVIRONMENT: # TODO make this to be decorator 
+    if not ENVIRONMENT: # TODO make this to be decorator
         return jsonify({"error": "Please provide MODE variable"})
-
     # TODO make this decorator also
     query = {
             "query" : {
                 "wildcard" : {
-                   "DS_Name" : "*{}*".format(request.json)
+                   "DS_Name" : "{}".format(request.json)
             }}}
     search = ES.search(index="documents", body=query)["hits"]["hits"]
     if not search:
         return jsonify([]), 200
 
     # instance of tree class
+
     tree = Tree(leafs=search)
-
     for file in tree.leafs:
+        tree.root = None
         node = tree.create_node(file)
-
+        nodes = []
+        nodes.append(node)
         # calling this function recursivly to create tree
-        structure = create_tree(tree)
-        tree.add_node(node)
-
+        structure = create_tree(tree, node)
+        tree.add_node(nodes)
         # resetting tree pointer to top of tree
         tree.pointer = tree.structure
 
     return jsonify(tree.structure), 200
 
 
-def create_tree(tree):
-    query = {"query": {"match": {"_id": tree.root}}}# can be function decorator whihc would pass query here 
-    #try:
-    search = ES.search(index="documents", body=query)["hits"]["hits"][0]
+def create_tree(tree, node=None):
+    nodes = []
+    query = {"query": {"match": {"_id": tree.root}}}# can be function decorator whihc would pass query here
+    search = ES.search(index="documents", body=query)["hits"]["hits"]
+    if ENVIRONMENT == 'files':
+        query = {
+                  "query" : {
+                    "bool" : {
+                      "must" : {
+                        "term" : { "DS_Parent" : tree.root }
+                      },
+                      "must_not" : {
+                        "term" : { "DS_Type": "file"}
+                }}}}
+        search_folders = ES.search(index="documents", body=query)["hits"]["hits"]
+        for folder in search_folders:
+            search.append(folder)
 
-    node = tree.create_node(search)
+    for file in search:
+        node = tree.create_node(file)
+        nodes.append(node)
 
     # base case for recursion
     if tree.root == 'null':
-        return tree.add_node(node)
-
+        return tree.add_node(nodes)
+    print(tree.structure)
     structure = create_tree(tree)
-    tree.add_node(node) 
+    tree.add_node(nodes)
     return structure
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def search_folders(tree):
-    """
-    Searching for additonal folders in case of
-    filse_and_folders MODE
-    """
-    query = {
-              "query": {
-                "bool" : {
-                  "must" : {
-                    "term" : { "DS_Parent" : tree.node['_id'] }
-                  },
-                  "must_not" : {
-                    "term": { "DS_Type": "file"}
-            }}}}
-    search = ES.search(index="documents", body=query)
-
-    for file in search['hits']['hits']:
-        if file['_id'] not in TREE_FILES:
-            tree.create_node(file)
-            tree.folders_list.append(tree.node)
-
 
 
 if __name__ == "__main__":
--- app/tree_service.py
+++ app/tree_service.py
@@ -1,14 +1,9 @@
-# global list of id’s to keep track of already added files to tree,
-# so we could skip searching for those which are not added
-TREE_FILES = ["null"] # TO-DO: make this redis list at some point
-
 
 class Tree:
     def __init__(self, leafs):
         self.root = None
         self.structure = []
         self.leafs = leafs
-        #self.folders_list = []
         self.pointer = None
 
     def create_node(self, data):
@@ -19,79 +14,35 @@ class Tree:
         node["_id"] = data["_id"]
         if node["DS_Type"] == "dir":
             node["children"] = []
-
-        self.root = node["DS_Parent"]
-
+        if not self.root:
+            self.root = node['DS_Parent']
+        elif node["DS_Parent"] != 'null' and self.root != 'null':
+            try:
+                self.root = int(node["DS_Parent"]) if int(self.root) > int(node["DS_Parent"]) else int(self.root)
+            except TypeError:
+                self.root = node['DS_Parent']
+        else:
+            self.root = 'null'
         return node
 
-    def add_node(self, obj):
+    def add_node(self, nodes):
         """
         Adding note to tree structure
         """
-        if not self.structure:
-            self.structure.append(obj)
-            self.pointer = self.structure
-        else:  
-            if obj['DS_Parent'] != self.pointer[0]['DS_Parent']:
+        for node in nodes:
+            if not self.structure:
+                self.structure.append(node)
+                self.pointer = self.structure
+            else:
+                if node['DS_Parent'] != self.pointer[0]['DS_Parent']:
+                    for file in self.pointer:
+                        if file['_id'] == node['DS_Parent']:
+                            self.pointer = file['children'] # here we add it to same list as previous one
+                # TODO this could be separate method
+                file_already_in_tree = False # TODO can this be better ???
                 for file in self.pointer:
-                    if file['_id'] == obj['DS_Parent']:
-                        self.pointer = file['children'] # here we add it to same list as previous one 
-                
-            # TODO this could be separate method
-            file_already_in_tree = False # TODO can this be better ???
-            for file in self.pointer:
-                if file['_id'] == obj['_id']:
-                    file_already_in_tree = True
-            if not file_already_in_tree:
-                self.pointer.append(obj) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def merge(self, master_item, branch_item):
-        """
-        Merging new branch to master which contans all 
-        previous branches
-        """
-        if not self.master:
-            self.master = self.branch
-        else:
-            pointer = None
-            while 'children' in branch_item:
-                for branch_file in branch_item['children']:
-                    for master_file in master_item['children']:
-                        if branch_file['_id'] == master_file['_id']:
-                            pointer = master_file
-                if self.folders_list: # in case filse_and_folders MODE
-                    self._add_folders(master_item)
-                if pointer:
-                    master_item = pointer
-                    branch_item = branch_file
-                    pointer = None
-                else:
-                    master_item['children'].append(branch_file)
-
-        self.branch = None
-
-    def _add_folders(self, path):
-        for folder in self.folders_list:
-            if folder['DS_Parent'] == path['_id'] and folder['_id'] not in TREE_FILES:
-                path['children'].append(folder)
-                TREE_FILES.append(folder['_id'])
+                    if file['_id'] == node['_id']:
+                        file_already_in_tree = True
+                        #break
+                if not file_already_in_tree:
+                    self.pointer.append(node)
